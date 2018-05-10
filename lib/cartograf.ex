ced13e21eedcc8baf462a7a76f6d9298405fc7d1
Devon Herlitz <devonherlitz@gmail.com>
2018-05-09 23:08:23 -0400

    added more docs, waiting to publish until I pin down the license, and decide on automapping

--- lib/cartograf.ex
+++ lib/cartograf.ex
@@ -1,13 +1,94 @@
 defmodule Cartograf do
   require IEx
 
+  @moduledoc """
+  Cartograf is a set of elixir macros for mapping fields from
+  one struct to another.
+  The goal is to make these struct-to-struct translations more
+  robust and less cumbersome to write and maintian.
+
+  # Basic Form
+  The basic form for using this module is of the form:
+  ```elixir
+  map Proj.A, Proj.B, :one_to_one do
+    [
+      let(:a, :aa),
+      let(:b, :bb),
+      let(:c, :cc),
+      let(:d, :dd)
+    ]
+  end
+  ```
+  This structure would create a function called `one_to_one/1`
+  within whatever module this the macro was invoked within.
+  The `one_to_one/1` function would expect a struct of type `Proj.A`
+  and return a struct of type `Proj.B` would be returned.
+  The struct returned would have the fields of the input struct, `A`,
+  mapped to the fields of the returned struct, `B`, thusly:
+
+
+  | `A`   |   to   | `B`   |
+  | ----- |:------:| ----- |
+  | `:a`  | &rarr; | `:aa` |
+  | `:b`  | &rarr; | `:bb` |
+  | `:c`  | &rarr; | `:cc` |
+  | `:d`  | &rarr; | `:dd` |
+
+  # Design Philosophy
+  Beyond the basic use, there are a number of options that can
+  be used within a `map` block beyond just the basic `let(from, to)`
+  form. However, it before introducing them, it is important to
+  understand the design philosophy and what this `cartograf` is
+  meant to do.
+
+  `cartograf` is supposed to be a tool, not a hazzard.
+  The point of this project is to create robust mappings from
+  one struct to another. As such, there are a few safeties in
+  place to protect the developer.
+    * `map()` does require that its input struct is of the
+    correct type. The function generated leverages pattern
+    matching on the argument to ensure that the struct
+    type is the one declared when the map was specified.
+    * All input fields must be handled. Each `map()`
+    will ensure that each field of the input is mentioned
+    in some capacity. If a field should not be included in
+    in the output struct, no problem, just include a
+    `drop(input_key)`. The main purpose for this is catch
+    instances where developers add fields to structs, but fail
+    to update the maps.
+
+  """
+
   defmacro __using__(_) do
     quote do
       import Cartograf
-      Module.register_attribute(__MODULE__, :already_set, accumulate: true)
     end
   end
 
+  @doc """
+  Creates a function in the the current module for mapping from
+  struct to another.
+
+  ```elixir
+  defmodule A, do: defstruct [:a, :b, :c]
+  defmodule B, do: defstruct [:aa, :bb, :cc]
+  defmodule YourModule do
+    use Cartograf
+    map A, B, :a_to_b do
+      [
+        let(:a, :aa),
+        let(:b, :bb),
+        let(:c, :cc)
+      ]
+    end
+  end
+  ```
+  ```elixir
+    iex> YourModule.a_to_b(%A{a: 1, b: "2", c: :d})
+    %B{aa: 1, bb: "2", cc: :d}
+  ```
+  """
+  @spec map(module(), module(), atom, [], do: any()) :: any()
   defmacro map(from_t, to_t, name, opts \\ [], do: block) do
     quote do
       def unquote(:"#{name}")(from = %unquote(from_t){}) do
@@ -71,7 +152,11 @@ defmodule Cartograf do
       end
     end)
   end
-
+  @doc """
+  Specify where the a field in the input should be mapped to
+  in the out.
+  """
+  @spec let(atom(), atom()) :: any()
   defmacro let(source_key, dest_key) do
     quote do
       fn from, to ->
@@ -80,7 +165,11 @@ defmodule Cartograf do
       end
     end
   end
-
+  @doc """
+  Allow for a field from the input to be excluded from
+  the output.
+  """
+  @spec drop(atom()) :: any()
   defmacro drop(source_key) do
     quote do
       fn from, to ->
@@ -89,6 +178,11 @@ defmodule Cartograf do
     end
   end
 
+  @doc """
+  Allow for a field in the output to be set to a constant
+  value.
+  """
+  @spec const(atom(), any()) :: any()
   defmacro const(dest_key, value) do
     quote do
       fn from, to ->
@@ -98,6 +192,10 @@ defmodule Cartograf do
     end
   end
 
+  @doc """
+  Used to specific a nested map within `map()`
+  """
+  @spec nest(module(), atom(), do: any()) :: any()
   defmacro nest(to_t, dest_key, do: block) do
     quote do
       fn from, to ->
--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Cartograf.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # Hex Config
+      description: description(),
+      package: package(),
+
+      # Exdoc Config
+      name: "Cartograf",
+      source_url: "https://github.com/Herlitzd/cartograf",
+      # The main page in the docs
+      docs: [main: "Cartograf", extras: ["README.md"]]
     ]
   end
 
@@ -20,6 +29,20 @@ defmodule Cartograf.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [{:ex_doc, "~> 0.16", only: :dev, runtime: false}]
   end
+
+  defp description, do: "A set of macros to help facilitate struct-to-struct field mapping"
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "cartograf",
+      # These are the default files included in the package
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Devon Herlitz"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/Herlitzd/cartograf"}
+    ]
+  end
+
 end
