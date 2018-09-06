781e1dbcbc99ff70d48c212d115e86e77cdbbb43
Devon Herlitz <2797382+Herlitzd@users.noreply.github.com>
2018-09-06 12:20:53 -0400

    Cleanup after v2 (#3)
    * refactored main file to pull atoms into module attributes, renamed methods to be more descriptive
    
    * combined advanced test files.

--- lib/mapping_exception.ex
+++ lib/mapping_exception.ex
@@ -1,4 +1,7 @@
 defmodule Cartograf.MappingException do
+  @moduledoc """
+  Exception to be reported when fields aren't mapped.
+  """
   @message "Field not mapped."
   defexception message: @message
 end
--- test/cartograf_advanced_test.exs
+++ test/cartograf_advanced_test.exs
@@ -48,6 +48,37 @@ defmodule CartografTest.Advanced do
     end
   end
 
+  map AA, AB, :with_const do
+    let :a, :aa
+    drop :b
+    const :bb, "abc"
+    let :c, :cc
+    let :d, :dd
+  end
+  map AA, AA, :with_missing, auto: true do
+    let :a, :a
+    drop :c
+  end
+  map AA, AA, :missing do
+    let :a, :a
+    drop :c
+    drop :b
+    drop :d
+  end
+
+  map AA, AB, :with_nest do
+    let :a, :aa
+    const :bb, "abc"
+    nest :cc, AA do
+      let :a, :a
+      let :b, :b
+    end
+    let :d, :dd
+    drop :b
+    drop :c
+  end
+
+
   test "simple nested mapping" do
     t = simple_nested(%AA{a: 1, b: 2, c: 3, d: 4})
     assert %AB{} = t
@@ -80,4 +111,34 @@ defmodule CartografTest.Advanced do
     assert t.dd.dd.bb.c == 3
     assert t.dd.dd.bb.d == 4
   end
+
+  test "with const" do
+    p = %AA{a: 1, b: 2, c: 3, d: 4}
+    k = with_const(p)
+    assert k.aa == 1
+    assert k.bb == "abc"
+    assert k.cc == 3
+    assert k.dd == 4
+  end
+
+  test "with nest" do
+    p = %AA{a: 1, b: 2, c: 3, d: 4}
+    k = with_nest(p)
+    assert k.aa == 1
+    assert k.bb == "abc"
+    assert %AA{} = k.cc
+    assert k.cc.a == 1
+    assert k.cc.b == 2
+    assert k.dd == 4
+  end
+
+  test "auto with drop" do
+    p = %AA{a: 1, b: 2, c: 3, d: 4}
+    k = with_missing(p)
+    assert k.a == 1
+    assert k.b == 2
+    assert k.c == nil
+    assert k.d == 4
+  end
+
 end
